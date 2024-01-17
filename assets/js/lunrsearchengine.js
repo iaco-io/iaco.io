0605d28e494fc65ecdb5a874f7c38cef33fad46a
IACO VIVACI <hi@iaco.io>
2024-01-17 09:51:07 +0100

    Fix problem with br inside h1

--- assets/js/lunrsearchengine.js
+++ assets/js/lunrsearchengine.js
@@ -27,7 +27,7 @@ var documents = [{
     }, {
     "id": 5,
     "url": "https://iaco.io/cmd/",
-    "title": "<span id='time' style='font-size: 40px;'></span><br><tok>[_jan-ali_kama-olin] [_wawa-insa_wile-awen_sona-jo]</tok>",
+    "title": "<span id='time' style='font-size: 40px;'></span><p><tok>[_jan-ali_kama-olin] [_wawa-insa_wile-awen_sona-jo]</tok></p>",
     "body": ""
     }, {
     "id": 6,
